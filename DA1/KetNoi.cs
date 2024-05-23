ccfb8c0c1795f2337e2d77bb8b2e059ad85cb6fe
UrosMora <khadajih369@gmail.com>
2024-05-24 00:10:50 +0700

    DA1

---
+++ DA1/KetNoi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLPP
+{
+
+	class KetNoi
+	{
+        public String connect = @"Data Source=DESKTOP-91HARMK;Initial Catalog=dulieu;Integrated Security=True;Encrypt=False";
+    }
+}
