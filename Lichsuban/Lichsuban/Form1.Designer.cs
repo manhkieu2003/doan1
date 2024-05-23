876a1b9f442b4efb22a122d81af6e5c049556a71
tliuu <109497229+Thuluu-coderlowmow@users.noreply.github.com>
2024-05-23 23:12:41 +0700

    Lịch sử bán

---
+++ Lichsuban/Lichsuban/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Lichsuban
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tim = new System.Windows.Forms.Button();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.mahd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(475, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lịch sử bán";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(930, 600);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tim
+            // 
+            this.tim.Location = new System.Drawing.Point(808, 600);
+            this.tim.Name = "tim";
+            this.tim.Size = new System.Drawing.Size(73, 41);
+            this.tim.TabIndex = 4;
+            this.tim.Text = "Tìm";
+            this.tim.UseVisualStyleBackColor = true;
+            this.tim.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(30, 181);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersWidth = 62;
+            this.dgv1.RowTemplate.Height = 28;
+            this.dgv1.Size = new System.Drawing.Size(542, 391);
+            this.dgv1.TabIndex = 20;
+            // 
+            // dgv2
+            // 
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Location = new System.Drawing.Point(586, 181);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.RowHeadersWidth = 62;
+            this.dgv2.RowTemplate.Height = 28;
+            this.dgv2.Size = new System.Drawing.Size(534, 391);
+            this.dgv2.TabIndex = 21;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(759, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(180, 26);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Chi tiết hóa đơn";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(220, 152);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(101, 26);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Hóa đơn";
+            // 
+            // mahd
+            // 
+            this.mahd.Location = new System.Drawing.Point(183, 93);
+            this.mahd.Name = "mahd";
+            this.mahd.Size = new System.Drawing.Size(205, 26);
+            this.mahd.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 26);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Mã hóa đơn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 672);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mahd);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dgv2);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.tim);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Lịch sử bán";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button tim;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridView dgv2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox mahd;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lichsuban/Lichsuban/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lichsuban
+{
+    public partial class Form1 : Form
+    {
+        static string sql = @"Data Source=MSI\SQLEXPRESS03;Initial Catalog=DA1;Integrated Security=True;Encrypt=False";
+        SqlConnection conn = new SqlConnection(sql);
+        SqlCommand cmd;
+        SqlDataAdapter dta;
+        DataSet ds;
+        DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void hienthihd()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                    string query = "select * from HoaDon";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dgv1.DataSource = dt;
+                }
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void hienthicthd()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                    string query = "select * from ChiTietHD";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    dgv2.DataSource = dt;
+                }
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Xác nhận thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            // Nếu người dùng không muốn thoát, hủy sự kiện đóng form
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (tim.Text == "Tìm")
+            {
+                try
+                {
+                    sql = "select * from HoaDon where (MaHD=@MaHD)";
+                    cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("MaHD", mahd.Text);
+                    //sql = "select * from ChiTietHD where (MaHD=@MaHD)";
+                    //cmd = new SqlCommand(sql, conn);
+                    //cmd.Parameters.AddWithValue("MaHD", mahd.Text);
+
+                    //sql = "select * from ChiTietHD where (MaSP=@MaSP)";
+                    //cmd = new SqlCommand(sql, conn);
+                    //                  cmd.Parameters.AddWithValue("MaSP", masp.Text);
+                    DataTable data = new DataTable();
+                    dta = new SqlDataAdapter(cmd);
+                    dta.Fill(data);
+                    if (data.Rows.Count > 0)
+                    {
+                        dgv1.DataSource = data;
+                        dgv1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                        tim.Text = "Dừng";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tìm thấy", "Tìm kiếm", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Không tìm thấy", "Tìm kiếm", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                //LoadData();
+                Form1_Load(sender, e);
+                tim.Text = "Tìm";
+            }
+        }
+        //private void LoadData()
+        //{
+        //    sql = "select * from HoaDon";
+        //    cmd = new SqlCommand(sql, conn);
+        //    dta = new SqlDataAdapter(cmd);
+        //    cmd.ExecuteNonQuery();
+        //    ds = new DataSet();
+        //    dta.Fill(ds);
+
+        //    sql = "select * from ChiTietHD";
+        //    cmd = new SqlCommand(sql, conn);
+        //    dta = new SqlDataAdapter(cmd);
+        //    cmd.ExecuteNonQuery();
+        //    ds = new DataSet();
+        //    dta.Fill(ds);
+
+        //    dgv1.DataSource = ds.Tables[0];
+        //    dgv1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hienthihd();
+            hienthicthd();
+        }
+    }
+}
