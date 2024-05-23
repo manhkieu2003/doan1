4b0f7d7ddd7a2f3284575d6d6d6cd644b631497b
manhkieu2003 <139309650+manhkieu2003@users.noreply.github.com>
2024-05-23 22:18:20 +0700

    login
    form đăng nhập

---
+++ WindowsFormsApp1/Chuongtrinhquanlixedapdien/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Chuongtrinhquanlixedapdien
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.btnExit = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bt_dangnhap = new Guna.UI2.WinForms.Guna2Button();
+            this.matkhau = new Guna.UI2.WinForms.Guna2TextBox();
+            this.tentk = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.White;
+            this.guna2Panel1.BorderRadius = 18;
+            this.guna2Panel1.Controls.Add(this.btnExit);
+            this.guna2Panel1.Controls.Add(this.label3);
+            this.guna2Panel1.Controls.Add(this.bt_dangnhap);
+            this.guna2Panel1.Controls.Add(this.matkhau);
+            this.guna2Panel1.Controls.Add(this.tentk);
+            this.guna2Panel1.Controls.Add(this.label1);
+            this.guna2Panel1.Controls.Add(this.guna2PictureBox1);
+            this.guna2Panel1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Panel1.ForeColor = System.Drawing.Color.Black;
+            this.guna2Panel1.Location = new System.Drawing.Point(406, 228);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(1112, 550);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnExit.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnExit.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnExit.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnExit.FillColor = System.Drawing.Color.White;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageSize = new System.Drawing.Size(30, 30);
+            this.btnExit.Location = new System.Drawing.Point(1065, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnExit.Size = new System.Drawing.Size(44, 38);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri Light", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LightGray;
+            this.label3.Location = new System.Drawing.Point(309, 500);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(449, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "*Bạn có chấp nhận các điều khoản và điều kiện của chúng tôi";
+            // 
+            // bt_dangnhap
+            // 
+            this.bt_dangnhap.BorderRadius = 18;
+            this.bt_dangnhap.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.bt_dangnhap.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.bt_dangnhap.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.bt_dangnhap.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.bt_dangnhap.FillColor = System.Drawing.Color.Lime;
+            this.bt_dangnhap.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_dangnhap.ForeColor = System.Drawing.Color.White;
+            this.bt_dangnhap.Location = new System.Drawing.Point(616, 398);
+            this.bt_dangnhap.Name = "bt_dangnhap";
+            this.bt_dangnhap.Size = new System.Drawing.Size(267, 51);
+            this.bt_dangnhap.TabIndex = 4;
+            this.bt_dangnhap.Text = "Đăng Nhập";
+            this.bt_dangnhap.Click += new System.EventHandler(this.bt_dangnhap_Click);
+            // 
+            // matkhau
+            // 
+            this.matkhau.BorderRadius = 18;
+            this.matkhau.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.matkhau.DefaultText = "";
+            this.matkhau.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.matkhau.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.matkhau.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.matkhau.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.matkhau.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.matkhau.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.matkhau.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.matkhau.IconLeft = ((System.Drawing.Image)(resources.GetObject("matkhau.IconLeft")));
+            this.matkhau.IconLeftSize = new System.Drawing.Size(30, 30);
+            this.matkhau.Location = new System.Drawing.Point(602, 296);
+            this.matkhau.Name = "matkhau";
+            this.matkhau.PasswordChar = '\0';
+            this.matkhau.PlaceholderText = "Enter password";
+            this.matkhau.SelectedText = "";
+            this.matkhau.Size = new System.Drawing.Size(307, 50);
+            this.matkhau.TabIndex = 3;
+            // 
+            // tentk
+            // 
+            this.tentk.BorderRadius = 18;
+            this.tentk.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tentk.DefaultText = "";
+            this.tentk.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.tentk.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.tentk.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.tentk.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.tentk.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.tentk.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.tentk.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.tentk.IconLeft = ((System.Drawing.Image)(resources.GetObject("tentk.IconLeft")));
+            this.tentk.IconLeftSize = new System.Drawing.Size(30, 30);
+            this.tentk.Location = new System.Drawing.Point(602, 211);
+            this.tentk.Name = "tentk";
+            this.tentk.PasswordChar = '\0';
+            this.tentk.PlaceholderText = "Enter name";
+            this.tentk.SelectedText = "";
+            this.tentk.Size = new System.Drawing.Size(307, 50);
+            this.tentk.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(667, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Đăng Nhập";
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(119, 171);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(309, 293);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1848, 759);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2TextBox tentk;
+        private Guna.UI2.WinForms.Guna2TextBox matkhau;
+        private Guna.UI2.WinForms.Guna2Button bt_dangnhap;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2CircleButton btnExit;
+    }
+}
+
---
+++ WindowsFormsApp1/Chuongtrinhquanlixedapdien/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Chuongtrinhquanlixedapdien
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            matkhau.PasswordChar = '*';
+        }
+
+        
+
+        private void bt_dangnhap_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-7HQBIC8;Initial Catalog=quanlicuahangxedapdien;Integrated Security=True;Encrypt=False");
+            try {
+
+                conn.Open();
+                string taikhoan = tentk.Text;
+                string matkhaus = matkhau.Text;
+                string sql = "select * from taikhoan where tentaikhoan='" + taikhoan + "' and matkhau='" + matkhaus + "'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader data = cmd.ExecuteReader();
+                if(data.Read()==true)
+                {
+                    MessageBox.Show("Đăng nhập thành công");
+                    Form3 f3 = new Form3();
+                    f3.Visible = true;
+                }
+                else
+                {
+                    MessageBox.Show("Đăng nhập thất bại");
+                }
+                
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Xác nhận thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            // Nếu người dùng không muốn thoát, hủy sự kiện đóng form
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
